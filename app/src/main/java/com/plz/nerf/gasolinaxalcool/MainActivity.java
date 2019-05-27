a4f38d54bfafe7b5c547cde1ebe5126fe1eb8f49
Joao <jvaoliveira96@gmail.com>
2019-05-27 11:36:21 -0300

    First commit on vitoro branch

--- app/src/main/java/com/plz/nerf/gasolinaxalcool/MainActivity.java
+++ app/src/main/java/com/plz/nerf/gasolinaxalcool/MainActivity.java
@@ -2,6 +2,8 @@ package com.plz.nerf.gasolinaxalcool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mAlc;
     private TextView mAlcPercent;
     private TextView mGasPercent;
-
+    private int editFlag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -29,13 +31,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mGas = findViewById(R.id.gasEditText);
-        mGas.setOnTouchListener(new View.OnTouchListener() {
+       mGas.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 mGas.setText("");
                 mGas.setBackgroundColor(getResources().getColor(R.color.grey));
                 mAlc.setBackgroundColor(getResources().getColor(R.color.grey));
+                editFlag = 0;
                 return false;
+            }
+        });
+
+        mGas.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(editFlag == 0){
+
+                    FuelPicker fp = new FuelPicker(mAlc.getText().toString(), mGas.getText().toString());
+                    mAlc.setText(fp.getEquivalentAlcoholPrice()+"");
+                    mAlcPercent.setText(fp.getAlcoholGasRelation()+"");
+
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(editFlag == 0){
+/*
+                    FuelPicker fp = new FuelPicker(mAlc.getText().toString(), mGas.getText().toString());
+                    mAlc.setText(fp.getEquivalentAlcoholPrice()+"");
+                    mAlcPercent.setText(fp.getAlcoholGasRelation()+"");*/
+
+
+                }
             }
         });
         mGasPercent = findViewById(R.id.gasPercentage);
@@ -47,7 +82,31 @@ public class MainActivity extends AppCompatActivity {
                 mAlc.setText("");
                 mGas.setBackgroundColor(getResources().getColor(R.color.grey));
                 mAlc.setBackgroundColor(getResources().getColor(R.color.grey));
+                editFlag = 1;
                 return false;
+            }
+        });
+        mAlc.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(editFlag == 1){
+                    FuelPicker fp = new FuelPicker(mAlc.getText().toString(), mGas.getText().toString());
+
+                    mGas.setText(fp.getEquivalentGasPrice()+"");
+                    mAlcPercent.setText(fp.getAlcoholGasRelation()+"");
+                    mAlc.setText(fp.getAlcoholPrice()+"");
+                }
             }
         });
         mAlcPercent = findViewById(R.id.alcoolPercentage);
